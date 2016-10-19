79f8f60ea4e7c8efdedb1d8bd09ef1c03cb1b00a
Ryan Woodcock <ryanguy426@users.noreply.github.com>
2016-10-19 13:52:15 -0400

    Add copyright notice

--- Golden Ticket/MainWindow.cs
+++ Golden Ticket/MainWindow.cs
@@ -1,4 +1,13 @@
-﻿using System;
+#region License
+// ====================================================
+// Golden Ticket Copyright(C) 2016 The Buzzy Project
+// This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+// and you are welcome to redistribute it under certain conditions; See
+// file LICENSE, which is part of this source code package, for details.
+// ====================================================
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
